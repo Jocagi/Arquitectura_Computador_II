af45ccefc9a698ee037fc820739fc7ff6c245efe
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-04-07 21:42:56 -0600

    LAB07

--- LAB07 - DATABASE/LAB07.py
+++ LAB07 - DATABASE/LAB07.py
@@ -1,6 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+import mariadb 
+
+#FUNCTION DEFINITION
+
+def insertIntoDatabase(date, activity):
+        cur.execute("INSERT INTO LAB07 (DATE, ACTIVITY) VALUES (?, ?)", (date, activity))
+        conn.commit()
+
+#DATABSE CONNECTION
+conn = mariadb.connect(
+    user="a72520_jose167",
+    password="XdJLB5U*x!-v.Pa",
+    host="mysql5030.site4now.net",
+    database="db_a72520_jose167")
+
+cur = conn.cursor()
 
 #GPIO CONFIGURATION
 sensor = 20
@@ -15,7 +31,7 @@ GPIO.setup(sensor, GPIO.IN)
 
 interruption = True
 
-print("LAB06 - Jose Giron")
+print("LAB07 - Jose Giron")
 
 while True:
         now = datetime.datetime.now()
@@ -24,8 +40,12 @@ while True:
         if GPIO.input(sensor) and not(interruption):
                 interruption = True
                 GPIO.output(led, True)
+                activity = "INTERRUPCION"
                 print("Interrupción en el sensor: " + date)
+                insertIntoDatabase(date, activity)
         elif not(GPIO.input(sensor)) and interruption:
                 interruption = False
                 GPIO.output(led, False)
+                activity = "CONTINUIDAD"
                 print("Continuidad en el sensor: " + date)
+                insertIntoDatabase(date, activity)
